0f67ffde05de4bc006ab31db73bfc0f20a5cd322
mark.wang2 <512354515@qq.com>
2018-04-21 10:11:54 +0800

    fix issues

--- app/assets/javascripts/search_form.js
+++ app/assets/javascripts/search_form.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
-    $("#select2input").select2();
+    $("#select2input").select2({placeholder: "请选择平台"});
 
     $("#select2input").on("select2:select", function (e) {
         $(".new-order-form").show();
     });
+
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+
+    if (getUrlParameter('bill_id')) { $(".new-order-form").show(); }
+
 });
