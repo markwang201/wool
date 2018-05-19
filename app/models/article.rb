625ac0c8ba0b7fd7952d984cf502f5cc17abfa09
mark.wang2 <512354515@qq.com>
2018-05-19 10:12:13 +0800

    add related articles wegeit

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,11 @@ class Article < ApplicationRecord
 
   def self.latest_fanli
     Article.all
-    #Article.where(topic: '2')
+    #Article.where(topic: '2'). Article.where(topic: topic) Article.where(topic: '2').order(:updated_at).first(3)
+  end
+
+  def self.related_articles(topic='')
+    Article.all
+    # Article.where(topic: topic) Article.where(topic: topic).order(:updated_at).first(3)
   end
 end
